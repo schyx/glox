bbf3f515b67fde21d3c1326b7c7d9a385b596f61
Sean Cheng <seanc2841@gmail.com>
2025-08-26 21:13:01 -0400

    refactor by function and then alphabetically

--- interpreter.go
+++ interpreter.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+// --------------- INTERPRETER ---------------
+
 type Interpreter struct {
 	output   any
 	err      error
@@ -23,182 +25,7 @@ func (interp *Interpreter) Interpret(statements []Stmt) {
 	}
 }
 
-func (interp *Interpreter) visitBinary(expr Binary) {
-	left, err, token := evalExpr(expr.left, interp.env)
-	if err != nil {
-		interp.output = nil
-		interp.err = err
-		interp.badToken = token
-		return
-	}
-	right, err, token := evalExpr(expr.right, interp.env)
-	if err != nil {
-		interp.output = nil
-		interp.err = err
-		interp.badToken = token
-		return
-	}
-
-	switch expr.operator.tokenType {
-	case BANG_EQUAL:
-		interp.output = !isEqual(left, right)
-		interp.err = nil
-	case EQUAL_EQUAL:
-		interp.output = isEqual(left, right)
-		interp.err = nil
-	case GREATER:
-		leftVal, rightVal, err := toFloatPair(left, right)
-		interp.getReturnVal(leftVal > rightVal, err, expr.operator)
-	case GREATER_EQUAL:
-		leftVal, rightVal, err := toFloatPair(left, right)
-		interp.getReturnVal(leftVal >= rightVal, err, expr.operator)
-	case LESS:
-		leftVal, rightVal, err := toFloatPair(left, right)
-		interp.getReturnVal(leftVal < rightVal, err, expr.operator)
-	case LESS_EQUAL:
-		leftVal, rightVal, err := toFloatPair(left, right)
-		interp.getReturnVal(leftVal <= rightVal, err, expr.operator)
-	case MINUS:
-		leftVal, rightVal, err := toFloatPair(left, right)
-		interp.getReturnVal(leftVal-rightVal, err, expr.operator)
-	case SLASH:
-		leftVal, rightVal, err := toFloatPair(left, right)
-		if err == nil && rightVal == 0 {
-			interp.getReturnVal(0, errors.New("Dividing by zero"), expr.operator)
-			return
-		}
-		interp.getReturnVal(leftVal/rightVal, err, expr.operator)
-	case STAR:
-		leftVal, rightVal, err := toFloatPair(left, right)
-		interp.getReturnVal(leftVal*rightVal, err, expr.operator)
-	case PLUS:
-		// numeric case
-		leftVal, rightVal, err := toFloatPair(left, right)
-		if err == nil {
-			interp.getReturnVal(leftVal+rightVal, err, expr.operator)
-			return
-		}
-		// string case
-		leftString, rightString, err := toStringPair(left, right)
-		interp.getReturnVal(leftString+rightString, err, expr.operator)
-	}
-}
-
-func (interp *Interpreter) getReturnVal(okVal any, err error, badToken Token) {
-	interp.err = err
-	interp.badToken = badToken
-	interp.output = okVal
-}
-
-func isEqual(left any, right any) bool {
-	if left == nil && right == nil {
-		return true
-	} else if left == nil {
-		return false
-	}
-	return left == right
-}
-
-func toStringPair(left any, right any) (string, string, error) {
-	leftString, leftErr := toString(left)
-	rightString, rightErr := toString(right)
-	if leftErr != nil {
-		return "", "", leftErr
-	} else if rightErr != nil {
-		return "", "", rightErr
-	} else {
-		return leftString, rightString, nil
-	}
-}
-
-func toFloatPair(left any, right any) (float64, float64, error) {
-	leftVal, leftErr := toFloat(left)
-	rightVal, rightErr := toFloat(right)
-	if leftErr != nil {
-		return 0, 0, leftErr
-	} else if rightErr != nil {
-		return 0, 0, rightErr
-	} else {
-		return leftVal, rightVal, nil
-	}
-}
-
-func (interp *Interpreter) visitGrouping(expr Grouping) {
-	interp.output, interp.err, interp.badToken = evalExpr(expr.expression, interp.env)
-}
-
-func (interp *Interpreter) visitLiteral(expr Literal) {
-	interp.output = expr.value
-}
-
-func (interp *Interpreter) visitLogical(expr Logical) {
-	left, leftErr, leftBadToken := evalExpr(expr.left, interp.env)
-	if leftErr != nil {
-		interp.err = leftErr
-		interp.badToken = leftBadToken
-		return
-	}
-	if expr.operator.tokenType == OR {
-		if isTruthy(left) {
-			interp.output = left
-			return
-		}
-	} else {
-		if !isTruthy(left) {
-			interp.output = left
-			return
-		}
-	}
-
-	right, rightErr, rightBadToken := evalExpr(expr.right, interp.env)
-	if rightErr != nil {
-		interp.err = rightErr
-		interp.badToken = rightBadToken
-		return
-	}
-	interp.output = right
-}
-
-func (interp *Interpreter) visitUnary(expr Unary) {
-	right, err, token := evalExpr(expr.right, interp.env)
-	if err != nil {
-		interp.output = nil
-		interp.err = err
-		interp.badToken = token
-		return
-	}
-	switch expr.operator.tokenType {
-	case BANG:
-		interp.output = !isTruthy(right)
-	case MINUS:
-		val, err := toFloat(right)
-		interp.output = -val
-		interp.err = err
-		interp.badToken = expr.operator
-	}
-}
-
-func isTruthy(object any) bool {
-	if object == nil {
-		return false
-	}
-	switch t := object.(type) {
-	case bool:
-		return t
-	default:
-		return true
-	}
-}
-
-func (interp *Interpreter) evaluate(expr Expr) {
-	expr.accept(interp)
-}
-
-func evalExpr(expr Expr, env *Environment) (any, error, Token) {
-	dummyInterp := Interpreter{env: env}
-	dummyInterp.evaluate(expr)
-	return dummyInterp.output, dummyInterp.err, dummyInterp.badToken
-}
+// --------------- STATEMENTS ---------------
 
 func (interp *Interpreter) execute(stmt Stmt) {
 	stmt.accept(interp)
@@ -210,6 +37,10 @@ func execStmt(stmt Stmt, env *Environment) (any, error, Token) {
 	return dummyInterp.output, dummyInterp.err, dummyInterp.badToken
 }
 
+func (interp *Interpreter) visitBlock(stmt Block) {
+	interp.executeBlock(stmt.statments, &Environment{values: make(map[string]any), enclosing: interp.env})
+}
+
 func (interp *Interpreter) executeBlock(statements []Stmt, env *Environment) {
 	for _, statement := range statements {
 		_, err, badToken := execStmt(statement, env)
@@ -219,10 +50,6 @@ func (interp *Interpreter) executeBlock(statements []Stmt, env *Environment) {
 			return
 		}
 	}
-}
-
-func (interp *Interpreter) visitBlock(stmt Block) {
-	interp.executeBlock(stmt.statments, &Environment{values: make(map[string]any), enclosing: interp.env})
 }
 
 func (interp *Interpreter) visitExpression(stmt Expression) {
@@ -302,6 +129,18 @@ func (interp *Interpreter) visitWhile(stmt While) {
 	}
 }
 
+// --------------- EXPRESSIONS ---------------
+
+func (interp *Interpreter) evaluate(expr Expr) {
+	expr.accept(interp)
+}
+
+func evalExpr(expr Expr, env *Environment) (any, error, Token) {
+	dummyInterp := Interpreter{env: env}
+	dummyInterp.evaluate(expr)
+	return dummyInterp.output, dummyInterp.err, dummyInterp.badToken
+}
+
 func (interp *Interpreter) visitAssign(expr Assign) {
 	value, err, badToken := evalExpr(expr.value, interp.env)
 	if err != nil {
@@ -319,6 +158,120 @@ func (interp *Interpreter) visitAssign(expr Assign) {
 	interp.output = value
 }
 
+func (interp *Interpreter) visitBinary(expr Binary) {
+	left, err, token := evalExpr(expr.left, interp.env)
+	if err != nil {
+		interp.output = nil
+		interp.err = err
+		interp.badToken = token
+		return
+	}
+	right, err, token := evalExpr(expr.right, interp.env)
+	if err != nil {
+		interp.output = nil
+		interp.err = err
+		interp.badToken = token
+		return
+	}
+	switch expr.operator.tokenType {
+	case BANG_EQUAL:
+		interp.output = !isEqual(left, right)
+		interp.err = nil
+	case EQUAL_EQUAL:
+		interp.output = isEqual(left, right)
+		interp.err = nil
+	case GREATER:
+		leftVal, rightVal, err := toFloatPair(left, right)
+		interp.getReturnVal(leftVal > rightVal, err, expr.operator)
+	case GREATER_EQUAL:
+		leftVal, rightVal, err := toFloatPair(left, right)
+		interp.getReturnVal(leftVal >= rightVal, err, expr.operator)
+	case LESS:
+		leftVal, rightVal, err := toFloatPair(left, right)
+		interp.getReturnVal(leftVal < rightVal, err, expr.operator)
+	case LESS_EQUAL:
+		leftVal, rightVal, err := toFloatPair(left, right)
+		interp.getReturnVal(leftVal <= rightVal, err, expr.operator)
+	case MINUS:
+		leftVal, rightVal, err := toFloatPair(left, right)
+		interp.getReturnVal(leftVal-rightVal, err, expr.operator)
+	case SLASH:
+		leftVal, rightVal, err := toFloatPair(left, right)
+		if err == nil && rightVal == 0 {
+			interp.getReturnVal(0, errors.New("Dividing by zero"), expr.operator)
+			return
+		}
+		interp.getReturnVal(leftVal/rightVal, err, expr.operator)
+	case STAR:
+		leftVal, rightVal, err := toFloatPair(left, right)
+		interp.getReturnVal(leftVal*rightVal, err, expr.operator)
+	case PLUS:
+		// numeric case
+		leftVal, rightVal, err := toFloatPair(left, right)
+		if err == nil {
+			interp.getReturnVal(leftVal+rightVal, err, expr.operator)
+			return
+		}
+		// string case
+		leftString, rightString, err := toStringPair(left, right)
+		interp.getReturnVal(leftString+rightString, err, expr.operator)
+	}
+}
+
+func (interp *Interpreter) visitGrouping(expr Grouping) {
+	interp.output, interp.err, interp.badToken = evalExpr(expr.expression, interp.env)
+}
+
+func (interp *Interpreter) visitLiteral(expr Literal) {
+	interp.output = expr.value
+}
+
+func (interp *Interpreter) visitLogical(expr Logical) {
+	left, leftErr, leftBadToken := evalExpr(expr.left, interp.env)
+	if leftErr != nil {
+		interp.err = leftErr
+		interp.badToken = leftBadToken
+		return
+	}
+	if expr.operator.tokenType == OR {
+		if isTruthy(left) {
+			interp.output = left
+			return
+		}
+	} else {
+		if !isTruthy(left) {
+			interp.output = left
+			return
+		}
+	}
+	right, rightErr, rightBadToken := evalExpr(expr.right, interp.env)
+	if rightErr != nil {
+		interp.err = rightErr
+		interp.badToken = rightBadToken
+		return
+	}
+	interp.output = right
+}
+
+func (interp *Interpreter) visitUnary(expr Unary) {
+	right, err, token := evalExpr(expr.right, interp.env)
+	if err != nil {
+		interp.output = nil
+		interp.err = err
+		interp.badToken = token
+		return
+	}
+	switch expr.operator.tokenType {
+	case BANG:
+		interp.output = !isTruthy(right)
+	case MINUS:
+		val, err := toFloat(right)
+		interp.output = -val
+		interp.err = err
+		interp.badToken = expr.operator
+	}
+}
+
 func (interp *Interpreter) visitVariable(expr Variable) {
 	val, err := interp.env.get(expr.name)
 	if err != nil {
@@ -328,6 +281,59 @@ func (interp *Interpreter) visitVariable(expr Variable) {
 		return
 	}
 	interp.output = val
+}
+
+// --------------- HELPERS ---------------
+
+func (interp *Interpreter) getReturnVal(okVal any, err error, badToken Token) {
+	interp.err = err
+	interp.badToken = badToken
+	interp.output = okVal
+}
+
+func isEqual(left any, right any) bool {
+	if left == nil && right == nil {
+		return true
+	} else if left == nil {
+		return false
+	}
+	return left == right
+}
+
+func toStringPair(left any, right any) (string, string, error) {
+	leftString, leftErr := toString(left)
+	rightString, rightErr := toString(right)
+	if leftErr != nil {
+		return "", "", leftErr
+	} else if rightErr != nil {
+		return "", "", rightErr
+	} else {
+		return leftString, rightString, nil
+	}
+}
+
+func toFloatPair(left any, right any) (float64, float64, error) {
+	leftVal, leftErr := toFloat(left)
+	rightVal, rightErr := toFloat(right)
+	if leftErr != nil {
+		return 0, 0, leftErr
+	} else if rightErr != nil {
+		return 0, 0, rightErr
+	} else {
+		return leftVal, rightVal, nil
+	}
+}
+
+func isTruthy(object any) bool {
+	if object == nil {
+		return false
+	}
+	switch t := object.(type) {
+	case bool:
+		return t
+	default:
+		return true
+	}
 }
 
 func toFloat(val any) (float64, error) {
